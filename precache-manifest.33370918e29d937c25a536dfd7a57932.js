640f0ceb21c0f10b18b3c4bde184bbe4d9baf643
Gurhan <83542792+gurhanalan@users.noreply.github.com>
2021-12-03 17:07:02 +0300

    Updates

---
+++ precache-manifest.33370918e29d937c25a536dfd7a57932.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a6e5793a53c31ff2584c8e1d38ed634",
+    "url": "/React-Review-Project/index.html"
+  },
+  {
+    "revision": "fe3278ee926ce750a9d5",
+    "url": "/React-Review-Project/static/css/main.3d9f8e8d.chunk.css"
+  },
+  {
+    "revision": "e1a3c50a15b44825207f",
+    "url": "/React-Review-Project/static/js/2.bdbc782b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Review-Project/static/js/2.bdbc782b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe3278ee926ce750a9d5",
+    "url": "/React-Review-Project/static/js/main.5a344eab.chunk.js"
+  },
+  {
+    "revision": "35f85b003a3d0ba8c710",
+    "url": "/React-Review-Project/static/js/runtime-main.c3d8485e.js"
+  }
+]);
